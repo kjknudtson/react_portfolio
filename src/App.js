c23cb735ca268d0acfd0bd7abaa95c1ce40079c1
Kyle <kjknudtson1989@gmail.com>
2020-09-26 08:50:58 -0500

    Moved index.html and changed button on homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -12,7 +12,12 @@ function Home() {
         <p className="lead homeContent">This is a website I made to showcase the various projects I've worked on.</p>
         <hr className="my-4" />
         <p className="homeContent">Use the navigation bar above, or the button below, to jump straight into my portfolio.</p>
-        <a className="btn btn-primary btn-lg" href="/portfolio" role="button">Let's go!</a>
+        <Link
+          to="/portfolio"
+          className="btn btn-primary btn-lg"
+        >
+          Let's go!
+        </Link>
       </div>
     
 
